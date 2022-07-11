964f0a12990de385a9422dbef40207ff04d436cf
darkhan_rauanovich <darhanamanbekov71@gmail.com>
2022-07-11 14:42:02 +0600

    editable title-description

--- src/components/TextInfo.vue
+++ src/components/TextInfo.vue
@@ -1,12 +1,39 @@
 <template>
-  <h2>{{ playlist.title }}</h2>
+  <div v-if="!editState">
+    <h2>{{ playlist.title }}</h2>
 
-  <p class="username">Created by {{ playlist.userName }}</p>
+    <p class="username">Created by {{ playlist.userName }}</p>
 
-  <p class="description">{{ playlist.description }}</p>
+    <p class="description">{{ playlist.description }}</p>
+  </div>
+  <div v-else>
+    <form @submit.prevent="handleSave">
+      <label for="title-edit">Title</label>
+      <input
+        type="text"
+        id="title-edit"
+        placeholder="title..."
+        required
+        v-model="title"
+      />
+      <label for="description-edit">Description</label>
+      <textarea
+        id="description-edit"
+        placeholder="desciption..."
+        required
+        v-model="description"
+      ></textarea>
+      <button v-if="!pendEdit">Save</button>
+      <button v-else disabled>Saving...</button>
+    </form>
+  </div>
 
   <div v-if="ownership">
-    <button class="edit">Edit</button>
+    <button class="edit" v-if="!editState" @click="editState = !editState">
+      Edit
+    </button>
+    <button class="edit" v-else @click="editState = !editState">Cancel</button>
+
     <button class="danger" @click="handleDelete" v-if="!isPending">
       Delete Playlist
     </button>
@@ -18,7 +45,7 @@
 import useDocument from "@/composables/useDocument";
 import useStorage from "@/composables/useStorage";
 
-import { ref } from "@vue/runtime-core";
+import { onMounted, ref } from "@vue/runtime-core";
 import { useRouter } from "vue-router";
 
 export default {
@@ -28,11 +55,25 @@ export default {
     id: {},
   },
   setup(props) {
-    const { deleteDocument } = useDocument("playlists", props.id);
+    const { deleteDocument, updateDocument } = useDocument(
+      "playlists",
+      props.id
+    );
     const { deleteImage } = useStorage();
+
     const router = useRouter();
 
     const isPending = ref(false);
+    const editState = ref(false);
+    const pendEdit = ref(false);
+
+    const title = ref("");
+    const description = ref("");
+
+    onMounted(() => {
+      title.value = props.playlist.title;
+      description.value = props.playlist.description;
+    });
 
     const handleDelete = async () => {
       const conf = confirm("Are you sure");
@@ -48,9 +89,38 @@ export default {
       }
     };
 
-    return { handleDelete, isPending };
+    const handleSave = async () => {
+      pendEdit.value = true;
+
+      await updateDocument({
+        title: title.value,
+        description: description.value,
+      });
+
+      editState.value = false;
+      pendEdit.value = false;
+    };
+
+    return {
+      handleDelete,
+      isPending,
+      editState,
+      handleSave,
+      title,
+      description,
+      pendEdit,
+    };
   },
 };
 </script>
 
-<style></style>
+<style scoped>
+form {
+  text-align: start;
+  margin-top: 20px;
+}
+form textarea {
+  resize: none;
+  height: 140px;
+}
+</style>
